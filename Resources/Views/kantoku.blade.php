7bf0cfb708d4b9e127c145643872d2db3166cc3f
Carsten <carsten@cogents.io>
2015-05-21 14:45:09 -0500

    info view

--- Resources/Views/kantoku.blade.php
+++ Resources/Views/kantoku.blade.php
@@ -1,49 +1,30 @@
-<html>
-	<head>
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-		<title>Module Management</title>
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				display: table;
-				font-weight: 400;
-				font-family: 'Open Sans', sans-serif;
-				color: #111;
-			}
+@extends('module_info')
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
+{{-- Web site Title --}}
+@section('title')
+{{ Config::get('general.title') }} :: @parent
+@stop
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
+@section('styles')
+@stop
 
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
+@section('scripts')
+@stop
 
-			.quote {
-				font-size: 24px;
-				text-align: left;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Kantoku</div>
-				<div class="quote">
-					Kantoku is a Rakko module that provides the ability to manage modules
-				</div>
+@section('inline-scripts')
+@stop
+
+
+{{-- Content --}}
+@section('content')
+
+	<div class="container">
+		<div class="content">
+			<div class="title">Kantoku</div>
+			<div class="quote">
+				Kantoku is a Rakko module that provides the ability to manage modules
 			</div>
 		</div>
-	</body>
-</html>
+	</div>
+
+@stop
